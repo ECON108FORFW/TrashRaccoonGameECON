f584a47632822c8c567f63312929c77de66b1134
ECON108FORFW <fwu25@u.rochester.edu>
2026-09-23 21:34:21 -0400

    Remove carry-load state

--- js/game-state.js
+++ js/game-state.js
@@ -3,12 +3,10 @@ export const gameState = {
   elapsedGameHours: 0,
   nightEnded: false,
   overlayOpen: false,
-  carryPounds: 0,
 };
 export function resetGameState() {
   gameState.session = { round: 0, results: [] };
   gameState.elapsedGameHours = 0;
   gameState.nightEnded = false;
   gameState.overlayOpen = false;
-  gameState.carryPounds = 0;
 }
